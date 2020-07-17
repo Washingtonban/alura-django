84498ce47e2b23a4e4f9a30fa4c4827812693e72
Washington Barbosa <wassuncaon@gmail.com>
2020-07-16 23:46:41 -0400

    [Feature] Continunado o curso de django
    Signed-off-by: Washington Barbosa <wassuncaon@gmail.com>

--- receitas/views.py
+++ receitas/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('<h1>Receitas</1>')
+    return render(request, 'index.html')
+
+def receita(request):
+    return render(request, 'receita.html')
